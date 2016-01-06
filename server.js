0c63df4cdc5606e20722a63b8cae47a922584471
ed42311 <ed42311@gmail.com>
2016-01-06 15:16:20 -0700

    added a few things to the index.html page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile('index.html')
+  res.sendFile('public/index.html')
   });
 
 app.listen(app.get('port'), function() {
