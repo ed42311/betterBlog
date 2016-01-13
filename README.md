347aeaff8dad3165bfb09da876cc4a24868d56f9
ed42311 <ed42311@gmail.com>
2016-01-13 15:28:31 -0700

    added some animation to the start of the blog

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ##This is a reinstating of the Agile-Citadel.
 
 This blog began with my code school adventure so long ago it seems.  
+
+Added a few things into the CSS files including an animation.css 
--- public/app.js
+++ public/app.js
@@ -1,3 +1,34 @@
-$(window).scroll(function(){
-  $(".title").css("opacity", 1 - $(window).scrollTop() / 350);
+$(window).scroll(function() {
+  $('#pic').each(function(){
+    var imagePos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+
+      if (imagePos < topOfWindow+400) {
+        $(this).addClass("slideRight");
+      }
+  });
 });
+
+$(window).scroll(function() {
+  $('#text').each(function(){
+    var imagePos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+
+      if (imagePos < topOfWindow+400) {
+        $(this).addClass("slideLeft");
+      }
+  });
+});
+
+var divs = $('.title');
+$(window).scroll(function(){
+if($(window).scrollTop()<250){
+divs.stop(true,true).fadeIn(1800);
+} else {
+divs.stop(true,true).fadeOut(2000);
+}
+$(".title").css("opacity", 1 - $(window).scrollTop() / 350);
+}); 
+
+
+
