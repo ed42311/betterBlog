624b80b9e8a35c9ebda6f4f2364497c5c6f87afa
ed42311 <ed42311@gmail.com>
2016-02-11 15:17:57 -0700

    changed port process to var port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
 var http = require('http');
+var port = process.env.PORT || 8020;
 var app = express();
 
-app.set('port', (process.env.PORT || 3020));
+
 
 app.use(express.static('public'));
 
@@ -11,6 +12,7 @@ app.get('/', function(req, res){
   res.not('public/index.html')
   });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+app.listen(port);
+
+console.log('Node app is running on port +' + port);
+
